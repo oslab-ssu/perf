1241bed82a40c94c2000197f96a04dea00a01d6c
poiskim <hgkim.oslab@gmail.com>
2026-06-15 07:19:55 +0900

    modify

--- src/run_record.sh
+++ src/run_record.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
+# todo
 
-mkdir -p result
-
-nsys profile --trace=osrt -o result/model_cpu python3 src/model_cpu.py
-nsys stats --report osrt_sum result/model_cpu.nsys-rep > result/summary.txt
--- src/run_stat.sh
+++ src/run_stat.sh
@@ -2,5 +2,5 @@
 
 mkdir -p result
 
-/usr/lib/linux-tools-6.8.0-124/perf stat -I 10 -e cycles,instructions,cache-misses,page-faults python3 src/model.py 2>result/perf_stat.log
+perf stat -I 10 -e cycles,instructions,cache-misses,page-faults python3 src/model.py 2>result/perf_stat.log
 python3 tool/graph.py result/perf_stat.log
